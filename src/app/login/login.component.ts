4ebf6c51f49b0d2196b61fb770dc1fa977ee0b51
RaiSMel <93801960+RaiSMel@users.noreply.github.com>
2022-09-06 20:27:52 -0300

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +10,7 @@ import { Observable} from 'rxjs';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
-  cep: Observable<any>;
+  cep: any;
 
   constructor(private http: HttpClient) { }
 
@@ -17,8 +18,10 @@ export class LoginComponent implements OnInit, OnDestroy {
     // With Routing in Ionic, The OnInit lifecycle hook 
     // may not get called consistently.
     console.log("LoginPage - OnInit")
-    this.cep = this.http.get('viacep.com.br/ws/01001000/json/');
-    console.log(this.cep)
+    this.http.get(`viacep.com.br/ws/01001000/json/`).subscribe(res=>{this.cep=res;
+  });
+   
+    
   }
 
   ngOnDestroy() {
